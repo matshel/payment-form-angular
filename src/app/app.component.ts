8a19d19cc2775e42952c0ba8bd6cbcce44674676
matshel <mats.helgesen@protonmail.com>
2022-02-25 03:51:30 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'payment-form';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+    console.log(this.name);
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
